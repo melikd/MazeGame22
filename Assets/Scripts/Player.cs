0868bffa9541b5b4a99f4e3186f3eb7a608a2ad0
melikd <44033097+melikd@users.noreply.github.com>
2018-11-14 19:48:32 +0200

    Maze Bullet-Enemy

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,17 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-	// members or variables
-	public float moveSpeed = 7.0f;
-	
-	//methods or functions
-		
-	// Update is called once per frame
-	void Update () {
-		//Player movement
-		if (Input.GetKey(KeyCode.W))
-		{
-			transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    // members or variables
+    public float moveSpeed = 7.0f;
+
+    //methods or functions
+
+    // Update is called once per frame
+    void Update() {
+        //Player movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            if (Input.GetKeyDown(KeyCode.W))
+                transform.GetChild(0).rotation = Quaternion.Euler(0, 0, 0);
+            //transform.GetChild(0).LookAt()
+        }
+        if (Input.GetKey(KeyCode.A)) { 
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            if (Input.GetKeyDown(KeyCode.S))
+                transform.GetChild(0).rotation = Quaternion.Euler(0, -90, 0);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+                transform.Translate(Vector3.back* moveSpeed * Time.deltaTime);
+            if (Input.GetKeyDown(KeyCode.S))
+                transform.GetChild(0).rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+         transform.Translate(Vector3.right* moveSpeed * Time.deltaTime);
+            if (Input.GetKeyDown(KeyCode.D))
+                transform.GetChild(0).rotation = Quaternion.Euler(0, 90, 0);
 		}
-	}
+        
+    }
 }
